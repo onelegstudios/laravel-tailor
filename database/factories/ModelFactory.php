9fbab22f7806ef6eab3c4118f3ecabb198697c02
Henrik Persson <henrik.persson@onelegstudios.com>
2026-06-25 13:43:28 +0200

    run configure script

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\Skeleton\Database\Factories;
+namespace Onelegstudios\Tailor\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
---
+++ src/Tailor.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace Onelegstudios\Tailor;
+
+class Tailor {}
